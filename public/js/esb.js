60c6035a4085f86c6cb9b727f39e0fbb39cdf4c2
Chris Riewaldt <c.riewaldt@gmail.com>
2020-10-05 20:08:34 -0400

    first cmd commit

---
+++ public/js/esb.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $('#bodyWrap').fadeIn(2000);
+    window.setInterval(function(){
+        $( ".vagaro-download" ).hide();
+    }, 500);
+});
+
---
+++ public/js/scroll.js
@@ -0,0 +1,2 @@
+//quick and dirtyy way to scroll the page to center the logo
+window.scrollBy(0, window.innerHeight / 4);
